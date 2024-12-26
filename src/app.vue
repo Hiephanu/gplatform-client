e64588a5ac336362974451b9ebc2dda7f6adbe74
Peter <gdschanu.vietpt0905@gmail.com>
2024-12-26 18:16:45 +0700

    feat: add thumbnail

--- src/app.vue
+++ src/app.vue
@@ -8,5 +8,21 @@
 </template>
 
 <script setup>
+useHead({
+  meta: [
+    // Open Graph metadata
+    { property: 'og:image', content: '/thumbnail.png' }, // Thumbnail image
+    { property: 'og:image:width', content: '1200' }, // Recommended width
+    { property: 'og:image:height', content: '630' }, // Recommended height
+    { property: 'og:url', content: 'https://www.gdghanu.com/' }, // Page URL
+    { property: 'og:type', content: 'website' }, // Type of content (e.g., website, article)
 
+    // Twitter metadata (optional, as it may help with cross-platform sharing)
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:image', content: '/thumbnail.png' },
+
+    // General image meta tag
+    { name: 'image', content: '/thumbnail.png' }
+  ]
+});
 </script>
